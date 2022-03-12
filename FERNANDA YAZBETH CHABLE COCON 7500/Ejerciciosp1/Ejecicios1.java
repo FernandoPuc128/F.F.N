18bd5396075139f46915fe1b59186fe3f4177122
Fernanda-Chable <80001938+Fernanda-Chable@users.noreply.github.com>
2022-03-12 14:57:57 -0600

    commi1

--- FERNANDA YAZBETH CHABLE COCON 7500/Ejerciciosp1/Ejecicios1.java
+++
@@ -1,5 +0,0 @@
-package FERNANDA YAZBETH CHABLE COCON 7500.Ejerciciosp1;
-
-public class Ejecicios1 {
-    
-}
--- FERNANDA YAZBETH CHABLE COCON 7500/Ejerciciosp1/EjeciciosP1/Ejercicio1/Ejercicio1.java
+++
@@ -1,5 +0,0 @@
-package FERNANDA YAZBETH CHABLE COCON 7500.Ejerciciosp1.EjeciciosP1.Ejercicio1;
-
-public class Ejercicio1 {
-    
-}
